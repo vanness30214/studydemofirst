8437deb2868f4324e2e07c8c1bdc85e86c639003
vanness30214 <vanness@vip.qq.com>
2019-04-08 23:39:34 +0800

    2019.4.8 插件DEMO

--- es6/demo/msg.js
+++ es6/demo/msg.js
@@ -1,16 +1,17 @@
-(function (window,document) {
-    let Msg =function (options){
+(function (window, document) {
+    let Msg = function (options) {
         this._init(options);
     };
-    Msg.prototype._init =function({content = ''}){
-        this.content =content;
+    Msg.prototype._init = function ({content = ''}) {
+        this.content = content;
         this._createElement();
-        this._show(this._el);
+        this._show([this._el, this._overlay]);
+        this._bind([this._el, this._overlay]);
     };
-    Msg.prototype._createElement =function (){
+    Msg.prototype._createElement = function () {
         let wrap = document.createElement('div');
-        wrap.className ='msg_wrap';
-        wrap.innerHTML ='  <div class="msg__wrap">\n' +
+        wrap.className = 'msg__wrap';
+        wrap.innerHTML =
             '    <div class="msg-header">\n' +
             '      <span>确认删除</span>\n' +
             '      <span class="msg-header-close-button">×</span>\n' +
@@ -24,13 +25,44 @@
             '    <div class="msg-footer">\n' +
             '      <button class="msg-footer-btn msg-footer-cancel-button">算了吧</button>\n' +
             '      <button class="msg-footer-btn msg-footer-confirm-button">好的</button>\n' +
-            '    </div>\n' +
-            '  </div>'
+            '    </div>';
+        let overlay = document.createElement('div');
+        overlay.className = "msg__overlay";
+        this._overlay = overlay;
+        this._el = wrap;
 
-            this._el =wrap;
-        Msg.prototype._show=function (el) {
-            document.body.appendChild(el)
-        }
+
     };
-    window.$Msg=Msg
-})(window,document);
+    Msg.prototype._bind = function ([el, overlay]) {
+        const hideMsg = function () {
+            el.style.transform = 'translate(-50%, -50%) scale(0, 0)';
+            overlay.style.opacity = '0';
+            setTimeout(function () {
+                document.body.removeChild(el);
+                document.body.removeChild(overlay);
+            },300);
+        };
+        const cancel = function (e) {
+            hideMsg();
+        };
+        const confirm = function (e) {
+            hideMsg();
+        };
+        overlay.addEventListener('click', cancel);
+        el.querySelector('.msg-header .msg-header-close-button').addEventListener('click', cancel);
+        el.querySelector('.msg-footer .msg-footer-cancel-button').addEventListener('click', cancel);
+        el.querySelector('.msg-footer .msg-footer-confirm-button').addEventListener('click', confirm);
+    };
+    Msg.prototype._show = function ([el, overlay]) {
+        document.body.appendChild(el);
+        document.body.appendChild(overlay);
+        setTimeout(function () {
+            el.style.transform = 'translate(-50%, -50%) scale(1, 1)';
+            overlay.style.opacity = '1';
+        })
+
+    };
+    window.$Msg = Msg
+})(window, document);
+
+
