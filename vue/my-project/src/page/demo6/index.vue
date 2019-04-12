e1bfe0532c4e9f4d3b157187144cf23dcd2dabac
vanness30214 <vanness@vip.qq.com>
2019-04-13 00:03:50 +0800

    2019.4.12 vue demostudy

--- vue/my-project/src/page/demo6/index.vue
+++ vue/my-project/src/page/demo6/index.vue
@@ -1,5 +1,23 @@
 <template>
-    
+    <div>
+      <ul id="example-1">
+        <li v-for="item in items">
+          {{item.message}}
+        </li>
+      </ul>
+      <ul id="example-2">
+        <li v-for="(item,index) in items">
+          {{index}}-{{item.message}}
+        </li>
+      </ul>
+      <ul id="example-3">
+        <li v-for="(value,key) in object">
+          {{key}}:{{value}}
+        </li>
+      </ul>
+      <div v-html="button" @click="btn1">1</div>
+    </div>
+
 </template>
 
 <script>
@@ -7,10 +25,24 @@
         name: "index",
       data(){
           return{
+            items:[
+              {message:'foo'},
+              {message:'goo'}
+            ],
+            object:{
+              firstname:'liu',
+              lastname:'oo',
+              age:14
+            },
+
+
+            btn:'cds',
 
           }
       },methods:{
-
+        btn1(){
+          console.log("asdsa")
+        }
       },computed:{
 
       }
