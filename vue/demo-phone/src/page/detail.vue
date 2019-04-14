4dd6ad5e3d9ce5580a555013395e72ad13a7b294
vanness30214 <vanness@vip.qq.com>
2019-04-14 23:11:53 +0800

    4.14 vue

---
+++ vue/demo-phone/src/page/detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="detail-page">
+    detail-page
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "detail"
+    }
+</script>
+
+<style scoped>
+  .detail-page{
+    width: 20px;
+  }
+</style>
---
+++ vue/demo-phone/src/page/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home-page">
+      home-pagedasdas
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
